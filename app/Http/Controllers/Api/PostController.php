48a953d1b058d58483feacbecf3094a240927243
Aung Ye Zaw <68589636+aelayyuu@users.noreply.github.com>
2022-10-20 03:02:06 +0630

    update

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
