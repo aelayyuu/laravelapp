61521494a171ac8c38cd679ac34346f72bd2093c
Aung Ye Zaw <68589636+aelayyuu@users.noreply.github.com>
2022-10-22 01:23:44 +0630

    updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     {
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
-            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'username' => 'required|string|max:255|unique:users',
             'phone_number' => 'required|numeric|digits:11',
             'password' => 'required|string|min:8',
         ]);
